1713e79ce1effc75b0af244c82a59d3fc2b9b783
Coild <xreza75@gmail.com>
2023-01-17 11:40:13 +0800

    membuat table dan controller untuk pembelian

---
+++ database/migrations/2023_01_17_031443_create_riwayats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRiwayatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riwayats', function (Blueprint $table) {
+            $table->id('riwayat_id');
+            $table->integer('riwayat_produk')->unique();
+            $table->string('riwwayat_jumlah')->nullable();
+            $table->string('riwayat_harga')->nullable();
+            $table->date('riwayat_waktu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riwayats');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Cproduk;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/produk',[Cproduk::class,'load'])->name('produk.load');
+Route::get('/produk/{id}',[Cproduk::class,'get'])->name('produk.get');
+Route::post('/produk',[Cproduk::class,'store'])->name('produk.store');
+Route::post('/produk/{id}',[Cproduk::class,'update'])->name('product.update');
+// Route::get('/generate_barcode/{id}', [Cproduk::class, 'generate_barcode'])->name('generate.barcode');
